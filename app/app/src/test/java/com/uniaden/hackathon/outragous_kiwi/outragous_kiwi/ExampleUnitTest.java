383d158053c1ba23382a3ccc65cf2c847ac730da
Isak <Isak@client235-168.wireless.umu.se>
2015-12-05 18:34:01 +0100

    Initial android project structure

---
+++ app/app/src/test/java/com/uniaden/hackathon/outragous_kiwi/outragous_kiwi/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.uniaden.hackathon.outragous_kiwi.outragous_kiwi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
